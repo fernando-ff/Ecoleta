e553082d05af223f464d18259ca32a035fea8ffa
fernandoff <fferreiralf@ufrn.edu.br>
2020-07-03 22:09:33 -0300

    server 1.0

---
+++ server/src/database/seeds/create_items.ts
@@ -0,0 +1,15 @@
+import Knex from 'knex';
+/**=========================
+ * Popula a tabela de items
+ * @param knex 
+ ============================*/
+export async function seed(knex : Knex) {
+    await knex('items').insert([
+        { title: 'Lâmpadas', image: 'lampadas.svg' },
+        { title: 'Pilhas e Baterias', image: 'baterias.svg' },
+        { title: 'Papéis e Papelão', image: 'papeus-papelao.svg' },
+        { title: 'Resíduos Eletrônicos', image: 'eletronicos.svg' },
+        { title: 'Resíduos Orgânicos', image: 'organicos.svg' },
+        { title: 'Óleo de Cozinha', image: 'oleo.svg' }
+    ]);
+}
